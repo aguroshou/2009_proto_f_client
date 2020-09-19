69664560e1efb03f89d79690d4ade7cada05e7e7
itsuka_to <itsuka@irvs.ait.kyushu-u.ac.jp>
2020-09-19 13:17:13 +0900

    playerと弾発射を作りました．

---
+++ Assets/itsuka-to/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 position;
+    private Vector3 screenToWorldPointPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3でマウス位置座標を取得する
+        position = Input.mousePosition;
+        // Z軸修正
+        position.z = 10f;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = screenToWorldPointPosition;
+    }
+}
