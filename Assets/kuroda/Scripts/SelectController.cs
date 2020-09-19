af785634b35a53b41e4f6beeb972e64946184d8f
黒田亮介 <>
2020-09-19 12:12:32 +0900

    no message

---
+++ Assets/kuroda/Scripts/SelectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectController : MonoBehaviour
+{
+    [SerializeField] PopButton inGameButton;
+    [SerializeField] PopButton MultiButton;
+
+    void Start()
+    {
+        inGameButton.Init(onSoloPlayButtonDown);
+        MultiButton.Init(onMultiPlayButtonDown);
+    }
+
+    private void onSoloPlayButtonDown()
+    {
+        SceneManager.LoadSceneAsync("IngameScene");
+    }
+
+    private void onMultiPlayButtonDown()
+    {
+        SceneManager.LoadSceneAsync("MatchingScene");
+    }
+}
---
+++ Assets/kuroda/Scripts/TitleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField] PopButton button;
+
+    void Start()
+    {
+        button.Init(onButtonDown);
+    }
+
+    private void onButtonDown()
+    {
+        SceneManager.LoadSceneAsync("SelectScene");
+    }
+}
