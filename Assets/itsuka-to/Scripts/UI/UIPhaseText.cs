86af0c160b26282e71dc30eafc297e386cc8bd95
itsuka_to <itsuka@irvs.ait.kyushu-u.ac.jp>
2020-09-19 17:21:11 +0900

    GameManagerを追加しました

---
+++ Assets/itsuka-to/Scripts/UI/UIPhaseText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class UIPhaseText : MonoBehaviour
+{
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        GameManager.Instance.Phase.Subscribe((phase) => {
+            text.text = "Phase : " + phase.ToString();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/itsuka-to/Scripts/UI/UIWaveText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class UIWaveText : MonoBehaviour
+{
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+
+        GameManager.Instance.Wave.Subscribe((wave) => {
+            text.text = "Wave : " + wave.ToString();
+        });
+    }
+}
