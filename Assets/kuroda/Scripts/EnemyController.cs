c2ff38e4a0ce01b1238c4b81d9914e390aa6ad07
黒田亮介 <>
2020-09-19 13:00:14 +0900

    敵の挙動

---
+++ Assets/kuroda/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int health;
+
+    private Vector3 targetpos;
+
+    void Start()
+    {
+        targetpos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time)*1.0f+ targetpos.x, targetpos.y, targetpos.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
