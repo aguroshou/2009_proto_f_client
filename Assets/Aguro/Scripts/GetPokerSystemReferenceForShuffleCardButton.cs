21f1a893e3d78c5696eadebbdf438ec88d71a593
aguroshou <aguroshou0413@gmail.com>
2020-09-20 12:13:32 +0900

    カードオブジェクトの参照を自動で追加する機能を実装しました

---
+++ Assets/Aguro/Scripts/GetPokerSystemReferenceForShuffleCardButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetPokerSystemReferenceForShuffleCardButton : MonoBehaviour
+{
+    GameObject pokerSystemGameObject;
+
+    void Start()
+    {
+        pokerSystemGameObject = GameObject.Find("PokerSystemGameObject");
+        Button button = GetComponent<Button>();
+        PokerSystem pokerSystem = pokerSystemGameObject.GetComponent<PokerSystem>();
+        button.onClick.AddListener(pokerSystem.ShuffleCards);
+    }
+}
