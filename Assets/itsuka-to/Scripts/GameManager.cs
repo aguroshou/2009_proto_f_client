50a1a52f4fed2cc91a54a3525715ff67437267a0
itsuka_to <itsuka@irvs.ait.kyushu-u.ac.jp>
2020-09-20 13:17:04 +0900

    ポーカー部分を結合しました！

--- Assets/itsuka-to/Scripts/GameManager.cs
+++ Assets/itsuka-to/Scripts/GameManager.cs
@@ -22,17 +22,26 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     /// </summary>
     public enum EGamePhase
     {
-        POKER_PHASE,
-        SHOOTING_PHASE,
-        STATUS_POWERUP_PHASE,
-        RESULT_PHASE,
+        POKER_PHASE,  // ポーカー
+        SHOOTING_PHASE,  // シューティング
+        STATUS_POWERUP_PHASE,  // ステータスあげる
+        RESULT_PHASE,  // 結果画面
     }
+
+
+    /// <summary>
+    /// ボス戦かどうか
+    /// </summary>
+    public ReactiveProperty<bool> IsBoss = new ReactiveProperty<bool>(false);
 
     /// <summary>
     /// 
     /// </summary>
     public ReactiveProperty<EGamePhase> Phase = new ReactiveProperty<EGamePhase>(EGamePhase.POKER_PHASE);
 
+    /// <summary>
+    /// ボス戦クリア以外の画面の遷移に移動する関数
+    /// </summary>
     public void ChangePhase()
     {
         if (Phase.Value == EGamePhase.POKER_PHASE)
@@ -41,19 +50,50 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         }
         else if (Phase.Value == EGamePhase.SHOOTING_PHASE)
         {
-            Phase.Value = EGamePhase.STATUS_POWERUP_PHASE;
+            if (IsBoss.Value)
+            {
+                // ボス戦の場合，ステータス強化画面にはいかず，ポーカーフェーズに向かう
+                Phase.Value = EGamePhase.POKER_PHASE;
+            }
+            else
+            {
+                Phase.Value = EGamePhase.STATUS_POWERUP_PHASE;
+            }
         }
         else if (Phase.Value == EGamePhase.STATUS_POWERUP_PHASE)
         {
-            if(Wave.Value < maxPhase)
+            if (IsBoss.Value)  // ボス戦
+            {
+                //　ボス戦ではステータス強化画面は存在しない
+                Debug.LogError("IsBoss Value Error");
+            }
+            else if(Wave.Value < maxPhase)
             {
                 Phase.Value = EGamePhase.POKER_PHASE;
                 Wave.Value += 1;
             }
             else
             {
-                Phase.Value = EGamePhase.RESULT_PHASE;
+                // ボス戦へ
+                Phase.Value = EGamePhase.POKER_PHASE;
+                IsBoss.Value = true;
             }
+        }
+    }
+
+    /// <summary>
+    /// ボス戦でクリアした時に呼ぶ関数
+    /// </summary>
+    public void BossClear()
+    {
+        // ボス戦かつシューティング面か？（バグ対策）
+        if(IsBoss.Value && Phase.Value == EGamePhase.SHOOTING_PHASE)
+        {
+            Phase.Value = EGamePhase.RESULT_PHASE;
+        }
+        else
+        {
+            Debug.LogError("ボスじゃない or シューティングフェーズではないのでクリアできません");
         }
     }
 
--- Assets/itsuka-to/Scripts/UI/UIWaveText.cs
+++ Assets/itsuka-to/Scripts/UI/UIWaveText.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using DG.Tweening;
 
 public class UIWaveText : MonoBehaviour
 {
     private Text text;
+    private RectTransform rectTran;
+    [SerializeField]
+    private float displayHeight = 200f;  // 表示時の幅
+    private Image image;
 
     private void Start()
     {
-        text = GetComponent<Text>();
+        rectTran = GetComponent<RectTransform>();
+        text = transform.Find("UIWaveTextChild").GetComponent<Text>();
+        image = GetComponent<Image>();
 
-        GameManager.Instance.Wave.Subscribe((wave) => {
-            text.text = "Wave : " + wave.ToString();
+        GameManager.Instance.Phase.Subscribe((phase) =>
+        {
+            if(phase == GameManager.EGamePhase.SHOOTING_PHASE)
+            {
+                // ボス戦開始時なら
+                if (GameManager.Instance.IsBoss.Value)
+                {
+                    TextChange("Boss Battle");
+                }
+                else
+                {
+                    TextChange("Wave : " + GameManager.Instance.Wave.Value.ToString());
+                }
+            }
+
+
         });
+    }
+
+    private void TextChange(string t)
+    {
+        text.text = t;
+        var sizeDelta = rectTran.sizeDelta;
+        sizeDelta.y = displayHeight;
+
+        image.color = new Color(0f, 0f, 0f, 1f);
+        rectTran.DOSizeDelta(sizeDelta, 1.0f).SetLoops(2, LoopType.Yoyo)
+            .OnComplete(()=> {
+                image.color = new Color(0f, 0f, 0f, 0f);
+            });
     }
 }
