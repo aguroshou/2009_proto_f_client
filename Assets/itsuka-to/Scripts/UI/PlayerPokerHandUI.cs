61ef0c187e86a7ccc4db100fba54e3f909f95fda
itsuka_to <itsuka@irvs.ait.kyushu-u.ac.jp>
2020-09-21 13:11:37 +0900

    テスト版

---
+++ Assets/itsuka-to/Scripts/UI/PlayerPokerHandUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class PlayerPokerHandUI : MonoBehaviour
+{
+    private Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        GameManager.Instance.playerPokerHand.Subscribe((hand) =>
+        {
+            text.text = "自分の役 : " + GameManager.EPokerHandToString(hand);
+        });
+    }
+}
