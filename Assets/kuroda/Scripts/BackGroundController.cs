f1b735debe56ff329f99618f5101a868d48a0d08
黒田亮介 <>
2020-09-21 12:50:40 +0900

    no message

---
+++ Assets/kuroda/Scripts/BackGroundController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    float rotation_speed = 1;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, this.rotation_speed);
+    }
+}
